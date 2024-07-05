62921c9ff4e617e3d6f45ec560b0d551bcb94e8e
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-07-05 21:44:37 +0530

    Add User and Role Model

---
+++ bookMyHotel/bookMyHotel/src/main/java/com/project/bookMyHotel/model/Role.java
@@ -0,0 +1,50 @@
+package com.project.bookMyHotel.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Collection<User> users = new HashSet<>();
+
+    public void assignRoleToUser(User user){
+        user.getRoles().add(this);
+        this.getUsers().add(user);
+    }
+
+    public void removeUserFromRole(User user){
+        user.getRoles().remove(this);
+        this.getUsers().remove(user);
+    }
+
+    public void removeAllUsersFromRole(){
+        if(getUsers() != null){
+            List<User> roleUsers = this.getUsers().stream().toList();
+            roleUsers.forEach(this :: removeUserFromRole);
+        }
+    }
+
+    public String getName(){
+        return name != null ? name : "";
+    }
+
+
+}
---
+++ bookMyHotel/bookMyHotel/src/main/java/com/project/bookMyHotel/model/User.java
@@ -0,0 +1,32 @@
+package com.project.bookMyHotel.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER,
+                cascade = {CascadeType.PERSIST,
+                           CascadeType.MERGE, CascadeType.DETACH})
+    @JoinTable(name = "user_Roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Collection<Role> roles = new HashSet<>();
+
+}
