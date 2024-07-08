3cdbd1fa2b220ef879096987fe32385301885028
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-07-08 21:43:27 +0530

    Code changes

--- bookMyHotel/bookMyHotel/src/main/java/com/project/bookMyHotel/controller/UserController.java
+++ bookMyHotel/bookMyHotel/src/main/java/com/project/bookMyHotel/controller/UserController.java
@@ -8,10 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +28,18 @@ public class UserController {
         try{
             User theUser = userService.getUser(email);
             return ResponseEntity.ok(theUser);
+        }catch(UsernameNotFoundException e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error fetching user");
+        }
+    }
+
+    @DeleteMapping("/delete/{userId}")
+    public ResponseEntity<String> deleteUser(@PathVariable("userId") String email){
+        try{
+            userService.deleteUser(email);
+            return ResponseEntity.ok("User deleted successfully");
         }catch(UsernameNotFoundException e){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }catch(Exception e){
