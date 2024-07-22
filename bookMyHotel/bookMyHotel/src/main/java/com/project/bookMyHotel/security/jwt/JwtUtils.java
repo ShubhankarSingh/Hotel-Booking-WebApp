3268f5c0546260f3e6d575f5eb3778ee64489d95
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-07-22 22:26:15 +0530

    JWT utils code

--- bookMyHotel/bookMyHotel/src/main/java/com/project/bookMyHotel/security/jwt/JwtUtils.java
+++ bookMyHotel/bookMyHotel/src/main/java/com/project/bookMyHotel/security/jwt/JwtUtils.java
@@ -1,8 +1,19 @@
 package com.project.bookMyHotel.security.jwt;
 
+import com.project.bookMyHotel.security.user.HotelUserDetails;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
 
 public class JwtUtils {
 
@@ -13,6 +24,23 @@ public class JwtUtils {
 
     @Value("${spring.security.jwt.expiration-time}")
     private int jwtExpirationTime;
+
+    public String generateJwtTokenFOrUser(Authentication authentication){
+        HotelUserDetails userPrincipal = (HotelUserDetails) authentication.getPrincipal();
+        List<String> roles = userPrincipal.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority).toList();
+
+        return Jwts.builder().setSubject(userPrincipal.getUsername())
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime()+jwtExpirationTime))
+                .signWith(key(),SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
 
 
 }
