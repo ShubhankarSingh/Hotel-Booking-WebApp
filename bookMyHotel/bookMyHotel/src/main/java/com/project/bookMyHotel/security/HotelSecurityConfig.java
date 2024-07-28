4b7903731c9a59909b0ae8a8b6880cd25ce5764c
Shubhankar Singh <singhshubhankar01@gmail.com>
2024-07-28 22:36:52 +0530

    Code changes

--- bookMyHotel/bookMyHotel/src/main/java/com/project/bookMyHotel/security/HotelSecurityConfig.java
+++ bookMyHotel/bookMyHotel/src/main/java/com/project/bookMyHotel/security/HotelSecurityConfig.java
@@ -1,12 +1,42 @@
 package com.project.bookMyHotel.security;
 
+import com.project.bookMyHotel.security.jwt.AuthTokenFilter;
+import com.project.bookMyHotel.security.jwt.JwtAuthEntryPoint;
+import com.project.bookMyHotel.security.user.HotelUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class HotelSecurityConfig {
+
+    private final HotelUserDetailsService userDetailsService;
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
+    @Bean
+    public AuthTokenFilter authenticationTokenFilter(){
+        return new AuthTokenFilter();
+    }
+
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+
+        return authConfig.getAuthenticationManager();
+
+    }
+
+    public DaoAuthenticationProvider authenticationProvider(){
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
